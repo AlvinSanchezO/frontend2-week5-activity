a52bab937d8f73315da416794429b47ce9b1091c
alvinsanchez93 <159292482+alvinsanchez93@users.noreply.github.com>
2026-04-16 09:02:07 -0700

    fix: formatear archivos de favoritos para cumplir con prettier

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 
 const GameCard = ({ game }) => {
-  
   const addToFavorites = (e) => {
     // IMPORTANTE: Evita que al dar clic al botón se active el Link de la tarjeta
     e.preventDefault();
@@ -16,21 +15,25 @@ const GameCard = ({ game }) => {
       localStorage.setItem('favorites', JSON.stringify(newFavs));
       alert(`❤️ ${game.name} guardado en favoritos`);
     } else {
-      alert("Este juego ya está en tu lista");
+      alert('Este juego ya está en tu lista');
     }
   };
 
   return (
     <div className="game-card-container">
       <Link to={`/game/${game.id}`} className="game-card">
-        <img src={game.background_image} alt={game.name} className="game-image" />
+        <img
+          src={game.background_image}
+          alt={game.name}
+          className="game-image"
+        />
         <div className="game-info">
           <h3 className="game-title">{game.name}</h3>
           <p className="game-rating">⭐ Calificación: {game.rating}</p>
         </div>
         {/* Botón de favorito */}
-        <button 
-          onClick={addToFavorites} 
+        <button
+          onClick={addToFavorites}
           className="btn-favorite"
           title="Agregar a favoritos"
         >
